ba72aebf405eb10c66de3ff8bde7d38bc1f638c8
Anthony Gitter <gitter@biostat.wisc.edu>
2024-10-25 10:40:07 -0500

    Move AF2 weights inside BindCraft dir

--- run_bindcraft.sh
+++ run_bindcraft.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# Get the AlphaFold2 weights
-mkdir -p ./params
-cp /staging/agitter/alphafold_params_2022-12-06.tar ./params/
-tar -xvf ./params/alphafold_params_2022-12-06.tar --directory ./params
-
 # Unpack the BindCraft code to the directory BindCraft-1.1.0
 tar -xvf bindcraft-v1.1.0.tar.gz
+
+# Get the AlphaFold2 weights
+mkdir -p ./BindCraft-1.1.0/params
+cp /staging/agitter/alphafold_params_2022-12-06.tar ./BindCraft-1.1.0/params/
+tar -xvf ./BindCraft-1.1.0/params/alphafold_params_2022-12-06.tar --directory ./BindCraft-1.1.0/params
 
 # Run BindCraft
 python3 -u ./BindCraft-1.1.0/bindcraft.py --settings './PDL1.json' --filters './default_filters.json' --advanced './4stage_multimer.json'
 
-# Remove AlphaFold2 weights
-rm -rf ./params
-
-# Remove BindCraft code
+# Remove BindCraft code and AlphaFold2 weights
 rm -rf ./BindCraft-1.1.0
